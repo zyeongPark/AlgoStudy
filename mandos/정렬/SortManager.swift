9ecce5bd6864d80defeb38fc6390f5256bfdf610
mandos1995 <mandos19950620@gmail.com>
2022-10-23 23:57:03 +0900

    ✨ 선택정렬, 삽입정렬 코드 추가

--- mandos/정렬/SortManager.swift
+++ mandos/정렬/SortManager.swift
@@ -15,6 +15,7 @@ final class SortManager<T: Comparable> {
     }
     
     func bubbleSort() {
+        array.shuffle()
         for i in 0..<array.count {
             for j in 0..<array.count - i - 1 {
                 if compare(array[j], array[j + 1]) {
@@ -22,7 +23,39 @@ final class SortManager<T: Comparable> {
                 }
             }
         }
+        print("==버블 정렬 결과==")
         array.forEach { print($0) }
+        print()
+    }
+    
+    func selectSort() {
+        array.shuffle()
+        for i in 0..<array.count {
+            var minIndex = i
+            for j in i + 1..<array.count {
+                if compare(array[minIndex], array[j]) {
+                    minIndex = j
+                }
+            }
+            array.swapAt(i, minIndex)
+        }
+        print("==선택 정렬 결과==")
+        array.forEach { print($0) }
+        print()
+    }
+    
+    func insertSort() {
+        array.shuffle()
+        for i in 1..<array.count {
+            for j in (1...i).reversed() {
+                if compare(array[j - 1], array[j]) {
+                    array.swapAt(j, j - 1)
+                } else { break }
+            }
+        }
+        print("==삽입 정렬 결과==")
+        array.forEach { print($0) }
+        print()
     }
 }
 
@@ -44,3 +77,26 @@ sortManager.bubbleSort()
 // SortData(index: 7, value: 9)
 // SortData(index: 8, value: 9)
 
+sortManager.selectSort()
+
+//SortData(index: 0, value: 1)
+//SortData(index: 6, value: 2)
+//SortData(index: 3, value: 4)
+//SortData(index: 2, value: 5)
+//SortData(index: 4, value: 7)
+//SortData(index: 5, value: 8)
+//SortData(index: 1, value: 9)
+//SortData(index: 7, value: 9)
+//SortData(index: 8, value: 9)
+
+sortManager.insertSort()
+
+//SortData(index: 0, value: 1)
+//SortData(index: 6, value: 2)
+//SortData(index: 3, value: 4)
+//SortData(index: 2, value: 5)
+//SortData(index: 4, value: 7)
+//SortData(index: 5, value: 8)
+//SortData(index: 1, value: 9)
+//SortData(index: 7, value: 9)
+//SortData(index: 8, value: 9)
